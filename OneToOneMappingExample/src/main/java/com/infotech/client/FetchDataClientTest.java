ba58064d8b0d341e5cfb5060b72b5b1737ac2bc7
Kishan Kumar <Kishan.javatrainer@gmail.com>
2017-10-11 21:48:39 +0530

    Code

---
+++ OneToOneMappingExample/src/main/java/com/infotech/client/FetchDataClientTest.java
@@ -0,0 +1,24 @@
+package com.infotech.client;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.infotech.entities.Address;
+import com.infotech.entities.Employee;
+import com.infotech.util.HibernateUtil;
+
+public class FetchDataClientTest {
+
+	public static void main(String[] args) {
+		Employee employee = null;
+	    try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+	    	employee = session.get(Employee.class, 1);
+	    	System.out.println(employee);
+	    	Address address = employee.getAddress();
+	    	System.out.println(address);
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	    
+	  }
+}
